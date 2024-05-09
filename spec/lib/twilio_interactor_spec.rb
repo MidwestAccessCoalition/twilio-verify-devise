5ab906fe7cad487489dd797077fe82f98d7ff099
Eric Schultz <eric+schultz@softwareforgood.com>
2024-05-08 19:41:54 -0500

    Move verify_client definition up in TwilioInteractor spec

--- spec/lib/twilio_interactor_spec.rb
+++ spec/lib/twilio_interactor_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe TwilioInteractor do
+  let(:verify_client) { double('verify_client') }
+
   describe '#register_totp' do
     context 'when register_totp_factor returns a valid factor' do
       it 'updates the mfa_config' do
-        verify_client = double('verify_client')
-
         mfa_config = create(:mfa_config)
         factor = double('factor', identity: 'an identity', sid: 'a sid', binding: {'uri' => 'https://qrcode_uri'})
 
@@ -28,8 +28,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when register_totp_factor raises' do
       it 'raises' do
-        verify_client = double('verify_client')
-
         mfa_config = create(:mfa_config)
 
         expect(verify_client).to receive(:register_totp_factor).with(
@@ -51,8 +49,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when update! raises' do
       it 'raises error' do
-        verify_client = double('verify_client')
-
         mfa_config = create(:mfa_config)
         factor = double('factor', identity: 'an identity', sid: 'a sid', binding: {'uri' => 'https://qrcode_uri'})
 
@@ -73,8 +69,6 @@ RSpec.describe TwilioInteractor do
   describe '#delete_entity' do
     context 'when identity is blank' do
       it 'does not call verify_client.delete_entity' do
-        verify_client = double('verify_client')
-
         expect(verify_client).to_not receive(:delete_entity)
 
         interactor = TwilioInteractor.new(verify_client)
@@ -85,8 +79,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when an error is raised' do
       it 'reraises the error from verify_client.delete_entity' do
-        verify_client = double('verify_client')
-
         expect(verify_client).to receive(:delete_entity).with('an id').and_raise('this is an error')
 
         interactor = TwilioInteractor.new(verify_client)
@@ -97,8 +89,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when an error is raised with a message that includes 20404/already deleted' do
       it "is falsey" do
-        verify_client = double('verify_client')
-
         expect(verify_client).to receive(:delete_entity).with('an id').and_raise("this includes 20404")
 
         interactor = TwilioInteractor.new(verify_client)
@@ -110,9 +100,7 @@ RSpec.describe TwilioInteractor do
     context 'when successful' do
       # from docs, it's not clear what is actually returned by deleteentity
       # https://www.twilio.com/docs/verify/api/entity#delete-an-entity-resource
-      it 'it is truthy' do 
-        verify_client = double('verify_client')
-
+      it 'it is truthy' do
         expect(verify_client).to receive(:delete_entity).with('an id').and_return(true)
 
         interactor = TwilioInteractor.new(verify_client)
@@ -146,7 +134,6 @@ RSpec.describe TwilioInteractor do
   describe '#totp_registration_valid?' do
     context 'when validate_totp_registration returns verified status' do
       it 'returns true' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -165,7 +152,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when validate_totp_registration returns a non-verified status' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -183,7 +169,6 @@ RSpec.describe TwilioInteractor do
     
     context 'when validate_totp_registration raises an error' do
       it 'reraises an error' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -201,7 +186,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when validate_totp_registration raises an error and it has 60306/ input token is too long' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -221,7 +205,6 @@ RSpec.describe TwilioInteractor do
   describe '#sms_token_valid?' do
     context 'when check_sms_verification_code returns approved status' do
       it 'returns true' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -239,7 +222,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when check_sms_verification_code returns a non-approved status' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -257,7 +239,6 @@ RSpec.describe TwilioInteractor do
     
     context 'when check_sms_verification_code raises an error' do
       it 'reraises the error' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -275,7 +256,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when check_sms_verification_code raises an error and it has 20404/ resource does not exist' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -293,7 +273,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when check_sms_verification_code raises an error and it has 60200/ token is too long' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -333,7 +312,6 @@ RSpec.describe TwilioInteractor do
   describe '#totp_login_valid?' do
     context 'when validate_totp_token returns approved status' do
       it 'returns true' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -351,7 +329,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when validate_totp_token returns a non-approved status' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -369,7 +346,6 @@ RSpec.describe TwilioInteractor do
     
     context 'when validate_totp_token raises an error' do
       it 'reraises the error' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -387,7 +363,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when validate_totp_token raises an error and it has 60318/ resource does not exist' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -406,7 +381,6 @@ RSpec.describe TwilioInteractor do
     context 'when validate_totp_token raises an error and it has 20404/ factor exists but wasnt validated during registration' do
 
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
@@ -424,7 +398,6 @@ RSpec.describe TwilioInteractor do
 
     context 'when validate_totp_token raises an error and it has 60200/ token is too long' do
       it 'returns false' do
-        verify_client = double('verify_client')
         mfa_config = build(:mfa_config)
         token = "token"
 
